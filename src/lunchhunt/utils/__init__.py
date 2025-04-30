7c5e28dc8cc9de7e5fd57c190a92c8114f182c71
Thomas R. Holy <thomas.holy@eah-jena.de>
2025-04-30 10:06:28 +0200

    Applied ruff fixes for __init__.py of utils.

--- src/lunchhunt/utils/__init__.py
+++ src/lunchhunt/utils/__init__.py
@@ -1,10 +1,13 @@
 from .util_functions import (
-    update_menu_categories, load_settings, create_cronjob, default_mensa_dict
+    create_cronjob,
+    default_mensa_dict,
+    load_settings,
+    update_menu_categories,
 )
 
 __all__ = [
+    "create_cronjob",
     "default_mensa_dict",
     "load_settings",
-    "update_menu_categories",
-    "create_cronjob"
+    "update_menu_categories"
 ]
