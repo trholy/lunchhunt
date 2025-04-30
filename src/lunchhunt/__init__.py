baa2a84ebf824a7f56df20e0d3e1c4bdbb9fdf03
Thomas R. Holy <thomas.holy@eah-jena.de>
2025-04-30 10:06:37 +0200

    Applied ruff fixes for __init__.py of lunchhunt.

--- src/lunchhunt/__init__.py
+++ src/lunchhunt/__init__.py
@@ -3,11 +3,11 @@ __author__ = "Thomas R. Holy"
 
 from .notify import Notifier
 from .scrap import MensaScraper
-from .utils import update_menu_categories, load_settings
+from .utils import load_settings, update_menu_categories
 
 __all__ = [
-    "Notifier",
     "MensaScraper",
-    "update_menu_categories",
-    "load_settings"
+    "Notifier",
+    "load_settings",
+    "update_menu_categories"
 ]
