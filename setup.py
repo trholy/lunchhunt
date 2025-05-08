a847f050ac58078785888399e84f173106c19535
Thomas R. Holy <thomas.holy@eah-jena.de>
2025-05-08 14:03:42 +0200

    Fixed encoding error when running setup.py.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="Thomas R. Holy",
     author_email="thomas.robert.holy@gmail.com",
     description="LunchHunt! Your Personal Food Concierge!",
-    long_description=open('README.md').read(),
+    long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     url="https://github.com/trholy/lunchhunt",
     packages=find_packages(where="src"),
